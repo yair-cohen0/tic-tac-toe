6a6757a8494cca0241bac273b16389ec1267effd
Yair Cohen <yair.cohen.mail@gmail.com>
2024-08-08 14:45:30 +0300

    created board

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { useCallback, useState } from 'react';
+import { Board } from './components/Board';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [board, setBoard] = useState(
+        Array(3)
+            .fill(null)
+            .map(() => Array(3).fill(null)),
+    );
+
+    const onMove = useCallback((x, y) => {
+        console.log(x, y);
+    }, []);
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <Board board={board} onMove={onMove}></Board>
         </>
     );
 }
---
+++ client/src/components/Board.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export function Board({ board, onMove }) {
+    return (
+        <_Board>
+            {board.map((row, y) => {
+                return row.map((cell, x) => (
+                    <Cell key={`${x},${y}`} onClick={() => onMove(x, y)}>
+                        {cell}
+                    </Cell>
+                ));
+            })}
+        </_Board>
+    );
+}
+
+const _Board = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    grid-template-rows: repeat(3, 100px);
+    gap: 5px;
+`;
+
+const Cell = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 100px;
+    background-color: #fff;
+    border: 2px solid #000;
+    font-size: 2em;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #f1f1f1;
+    }
+`;
